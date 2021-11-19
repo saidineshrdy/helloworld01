5799b0d27bfa5e2201cffb1c661bd06077d90545
saidineshrdy <94595536+saidineshrdy@users.noreply.github.com>
2021-11-19 17:00:24 +0530

    first commit in main

--- src/main/java/com/cg/springboot/SpringbootHelloApplication.java
+++ src/main/java/com/cg/springboot/SpringbootHelloApplication.java
@@ -8,7 +8,7 @@ public class SpringbootHelloApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootHelloApplication.class, args);
-		System.out.println("hello");
+		System.out.println("hello world");
 	}
 
 }
